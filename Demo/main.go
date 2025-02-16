5811e696e984348b1e03d8f71161eb6c034d0e2e
dinshasivan <dinshasivan@gmail.com>
2025-02-16 21:24:48 +0530

    📦️ | add packages

--- Demo/main.go
+++ Demo/main.go
@@ -2,49 +2,53 @@ package main
 
 import (
 	"Demo/utils"
-	
-	// "fmt"
-	// "os"
+	"fmt"
+	"os"
 )
 
 func main() {
-	// fmt.Println("Hello world")
-	// utils.Helloworld()
-	// utils.VariableDeclaration()
-	// utils.FormatingString()
-	// utils.ArrayOperation()
-	// utils.Libraries()
-	// utils.Loops()
-	// utils.Conditions()
+	fmt.Println("Hello world")
 
-	// utils.Functions("dinsha")
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide an argument to execute a specific function.")
+		return
+	}
 
-	// utils.CycleName([]string{"cloud","tifa","barret"},utils.Functions)
-
-	// a1 := utils.CircleArea(10.5)
-	// a2 := utils.CircleArea(15)
-
-	// fmt.Println(a1,a2)
-	// fmt.Printf("cercle 1 is %0.3f and circle 2 is %0.3f \n",a1, a2)
-
-	// if len(os.Args) > 1 {
-	// 	if os.Args[1] == "gm" {
-	// 		utils.Functions("dinsha")
-	// 	} else if os.Args[1] == "gn" {
-	// 		utils.Bye("sree")
-	// 	}
-	// } else {
-	// 	fmt.Println("Nothing to do")
-	// }
-
-	// fn, sn := utils.MultipleReturnValue("dinsha sivan")
-	// fmt.Println(fn,sn)
-
-	// utils.Sum(1,2,3,4,5)
-
-	// utils.MapOerations()
-
-	// utils.PassByValue()
-
-	utils.ReceiverFunction()	
+	switch os.Args[1] {
+	case "1":
+		utils.Helloworld()
+	case "2":
+		utils.VariableDeclaration()
+	case "3":
+		utils.FormatingString()
+	case "4":
+		utils.ArrayOperation()
+	case "5":
+		utils.Libraries()
+	case "6":
+		utils.Loops()
+	case "7":
+		utils.Conditions()
+	case "8":
+		utils.Functions("dinsha")
+	case "9":
+		utils.CycleName([]string{"cloud", "tifa", "barret"}, utils.Functions)
+	case "10":
+		a1 := utils.CircleArea(10.5)
+		a2 := utils.CircleArea(15)
+		fmt.Printf("Circle 1: %0.3f, Circle 2: %0.3f\n", a1, a2)
+	case "11":
+		fn, sn := utils.MultipleReturnValue("dinsha sivan")
+		fmt.Println(fn, sn)
+	case "12":
+		utils.Sum(1, 2, 3, 4, 5)
+	case "13":
+		utils.MapOerations()
+	case "14":
+		utils.PassByValue()
+	case "15":
+		utils.ReceiverFunction()
+	default:
+		fmt.Println("Invalid argument. Please provide a valid function name.")
+	}
 }
